8e89ca041ee6d0fe0b685d76f96ee4b589f79c1f
jonath92 <jonathanheard92@gmail.com>
2021-08-29 11:56:53 +0200

    working with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 const path = require('path');
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: "eval-source-map",
+  target: 'node', // without webpack renames 'global'
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
